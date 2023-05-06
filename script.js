28e382647863da763dc8ed09c945b47cf87e0e74
Sbastdia <sbastdia@myuax.com>
2023-05-06 12:56:25 +0200

    mejoras

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-var audio = document.getElementById("audioFlamenco");
-audio.play();
+var audio1 = document.getElementById("audioFlamenco");
+audio1.play();
 
 // Agrega un evento clic al botón para pausar el audio
 document.getElementById("pauseButton").addEventListener("click", function() {
-    audio.pause();
+    audio1.pause();
 });
 
 // Agrega un evento de cambio al deslizador de volumen para cambiar el volumen del audio
 document.getElementById("volumeSlider").addEventListener("change", function() {
-    audio.volume = this.value;
+    audio1.volume = this.value;
+});
+
+var audio2 = document.getElementById("audioBallet");
+audio2.play();
+
+// Agrega un evento clic al botón para pausar el audio
+document.getElementById("pauseButton").addEventListener("click", function() {
+    audio2.pause();
+});
+
+// Agrega un evento de cambio al deslizador de volumen para cambiar el volumen del audio
+document.getElementById("volumeSlider").addEventListener("change", function() {
+    audio2.volume = this.value;
+});
+
+var audio3 = document.getElementById("audioContemporaneo");
+audio3.play();
+
+// Agrega un evento clic al botón para pausar el audio
+document.getElementById("pauseButton").addEventListener("click", function() {
+    audio3.pause();
+});
+
+// Agrega un evento de cambio al deslizador de volumen para cambiar el volumen del audio
+document.getElementById("volumeSlider").addEventListener("change", function() {
+    audio3.volume = this.value;
 });
