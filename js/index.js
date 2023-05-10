7990468f5eff92c4dd96fbc11490f8164835a4d0
Sbastdia <sbastdia@myuax.com>
2023-05-10 12:08:25 +0200

    hecho

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const imagen = document.getElementById("img1");
+const imagenUrlNueva = "https://img.freepik.com/vector-premium/diseno-silueta-baile-personas-bailando_566661-15790.jpg?w=2000";
+
+// Agregar el evento "mouseover" a la imagen original
+imagen.addEventListener("mouseover", () => {
+    // Crear una nueva imagen con la URL de la imagen nueva
+    const nuevaImagen = new Image();
+    nuevaImagen.src = imagenUrlNueva;
+
+    // Reemplazar la imagen original con la nueva imagen
+    imagen.src = nuevaImagen.src;
+
+    // Agregar el evento "mouseout" a la imagen original para restaurar la original
+    imagen.addEventListener("mouseout", () => {
+        imagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4ouxJRHg09wFHJyT6g6ZIGKiFNHRtVYHr0Q&usqp=CAU";
+    });
+});
+
+
+
 // Seleccionar todos los elementos con la clase "pregunta"
 const preguntas = document.querySelectorAll(".pregunta");
 
