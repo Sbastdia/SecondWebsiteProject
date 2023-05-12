a7d7c3b698c1248e344c2a78b9a43eb6535470b1
Sbastdia <sbastdia@myuax.com>
2023-05-12 18:15:58 +0200

    ..

--- js/quiz.js
+++ js/quiz.js
@@ -3,36 +3,44 @@ let quizForm = document.getElementById("quiz");
 let resultsDiv = document.getElementById("results");
 //Inicializamos la puntuación del usuario
 let score = 0;
+//Inicializamos una variable booleana para saber si el usuario ya envió el formulario y no volver a enviarlo
+let quizSubmitted = false;
 
 //Agregamos un event listener para cuando el usuario envíe el formulario
 quizForm.addEventListener("submit", function(event) {
-  event.preventDefault(); //Evita que la página se recargue al enviar el formulario
+    event.preventDefault(); //Evita que la página se recargue al enviar el formulario
 
-    //Obtenemos las respuestas seleccionadas por el usuario
-    let q1Answer = quizForm.elements.q1.value;
-    let q2Answer = quizForm.elements.q2.value;
-    let q3Answer = quizForm.elements.q3.value;
-    let q4Answer = quizForm.elements.q4.value;
+    //Verificamos si el formulario ya ha sido enviado
+    if (!quizSubmitted) {
+        //Obtenemos las respuestas seleccionadas por el usuario
+        let q1Answer = quizForm.elements.q1.value;
+        let q2Answer = quizForm.elements.q2.value;
+        let q3Answer = quizForm.elements.q3.value;
+        let q4Answer = quizForm.elements.q4.value;
 
-    //Comparamos las respuestas seleccionadas con las respuestas correctas
-    if (q1Answer === "b") {
-        score += 1;
-    }
-    if (q2Answer === "a") {
-        score += 1;
-    }
-    if (q3Answer === "b") {
-        score += 1;
-    }
-    if (q4Answer === "c") {
-        score += 1;
-    }
+        //Comparamos las respuestas seleccionadas con las respuestas correctas
+        if (q1Answer === "b") {
+            score += 1;
+        }
+        if (q2Answer === "a") {
+            score += 1;
+        }
+        if (q3Answer === "b") {
+            score += 1;
+        }
+        if (q4Answer === "c") {
+            score += 1;
+        }
 
-    //Mostramos la puntuación del usuario
-    if (score === 4) {
-        resultsDiv.innerHTML = "¡Felicidades! ¡Has obtenido la puntuación máxima!";
-    } else {
-        resultsDiv.innerHTML = `¡Casi! Tu puntuación es: ${score} / 4`;
+        //Actualizamos la variable booleana
+        quizSubmitted = true;
+
+        //Mostramos la puntuación del usuario
+        if (score === 4) {
+            resultsDiv.innerHTML = "¡Felicidades! ¡Has obtenido la puntuación máxima!";
+        } else {
+            resultsDiv.innerHTML = `¡Casi! Tu puntuación es: ${score} / 4`;
+        }
     }
 });
 
